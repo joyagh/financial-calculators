d01144603c56fedc0c8eb8ae4b3eec34a233345e
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-10 13:38:47 -0400

    bug fix

--- src/main/java/com/pluralsight/MortgageCalculator.java
+++ src/main/java/com/pluralsight/MortgageCalculator.java
@@ -10,20 +10,32 @@ public class MortgageCalculator {
 
        // ask user for principal amount
 
+        System.out.println("Mortgage Calculator");
+        System.out.println("Lets figure out your monthly payment and interest.");
+
         System.out.print("Please enter principal amount: ");
         double principal = scan.nextDouble();
 
 
         System.out.print("Please enter interest rates: ");
-        double intRate = scan.nextDouble();
+        double intRate = scan.nextDouble()/ 100;
 
 
         System.out.print("Please enter loan length: ");
         int loanLength = scan.nextInt();
 
+        intRate /= 12;
+
+        double numOfPayments = 12 * loanLength;
+
         System.out.println(principal);
         System.out.println(intRate);
         System.out.println(loanLength);
+
+       // M = P × (i * (1 + i)^n / ( (1 + i)^n ) - 1)
+        double monthlyPayment = principal * (intRate * Math.pow(1 + intRate, numOfPayments)) / (Math.pow(1 + intRate, numOfPayments) -1);
+
+        System.out.println(monthlyPayment);
 
     }
 
